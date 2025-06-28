1e5d0d10370efa16c70d239c8edf15dc3e80e707
TheLunaFoxx <lunafoxx2024@gmail.com>
2025-06-28 16:52:01 +0100

    Update gui_viewer.py

--- gui_viewer.py
+++ gui_viewer.py
@@ -59,7 +59,7 @@ def index():
     return render_template_string(TEMPLATE, frees=frees)
 
 def run_gui():
-    app.run(host="0.0.0.0", port=6969)
+    app.run(host="0.0.0.0", port=8080)
 
 if __name__ == "__main__":
     run_gui()
